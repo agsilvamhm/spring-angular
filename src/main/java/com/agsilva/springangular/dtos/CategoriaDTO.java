0f5e7c323f148fd7c777280946d537f3f66a08b7
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-10-27 19:59:52 -0300

    Atualizando aula 12

---
+++ src/main/java/com/agsilva/springangular/dtos/CategoriaDTO.java
@@ -0,0 +1,47 @@
+package com.agsilva.springangular.dtos;
+
+import com.agsilva.springangular.domain.Categoria;
+
+import java.io.Serializable;
+
+public class CategoriaDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String nome;
+    private String descricao;
+
+    public CategoriaDTO() {
+        super();
+    }
+
+    public CategoriaDTO(Categoria obj) {
+        super();
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.descricao = obj.getDescricao();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
--- src/main/java/com/agsilva/springangular/service/CategoriaService.java
+++ src/main/java/com/agsilva/springangular/service/CategoriaService.java
@@ -6,6 +6,7 @@ import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class CategoriaService{
     public Categoria findById(Integer id){
         Optional<Categoria> obj = repository.findById(id);
         return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! id:" + id + "",  "Tipo:" + Categoria.class.getName()));
+    }
+
+    public List<Categoria> findAll(){
+        return repository.findAll();
     }
 }
