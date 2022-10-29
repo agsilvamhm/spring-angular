847eeb93cc5c775fae3771e86f976f202c4d624e
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-10-28 23:59:54 -0300

    Atualizando aula 18

--- src/main/java/com/agsilva/springangular/service/LivroService.java
+++ src/main/java/com/agsilva/springangular/service/LivroService.java
@@ -6,6 +6,8 @@ import com.agsilva.springangular.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.Email;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,8 +16,16 @@ public class LivroService {
     @Autowired
     private LivroRepository repository;
 
+    @Autowired
+    private CategoriaService categoriaService;
+
     public Livro findById(Integer id){
         Optional<Livro> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: " + id + ", Tipo: " + Livro.class.getName()));
+    }
+
+    public List<Livro> findAll(Integer id_cat){
+        categoriaService.findById(id_cat);
+        return repository.findAllCategoria(id_cat);
     }
 }
