61241c7f78f56ef0e8ab0a28a4ea70d97286d8b6
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-10-29 20:37:49 -0300

    Gravando a aula 23

--- src/main/java/com/agsilva/springangular/exceptions/RecourceExceptionHandler.java
+++ src/main/java/com/agsilva/springangular/exceptions/RecourceExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.agsilva.springangular.exceptions;
 
+import org.springframework.boot.context.properties.bind.validation.ValidationErrors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,6 +22,15 @@ public class RecourceExceptionHandler {
     @ExceptionHandler(DataIntegrityVioletionException.class)
     public ResponseEntity<StandardError> dataIntegrityVioletionException(DataIntegrityVioletionException e, ServletRequest request){
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException e, ServletRequest request){
+        ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Erro na validação dos campos!");
+        for(FieldError x : e.getBindingResult().getFieldErrors()){
+            error.addErrors(x.getField(), x.getDefaultMessage());
+        }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
