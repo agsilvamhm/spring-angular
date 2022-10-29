78a0878cb3cc6ff7124f2c7897c97f41fd8200ab
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-10-29 10:41:17 -0300

    Salvando a aula 20

--- src/main/java/com/agsilva/springangular/service/LivroService.java
+++ src/main/java/com/agsilva/springangular/service/LivroService.java
@@ -1,6 +1,7 @@
 package com.agsilva.springangular.service;
 
 import com.agsilva.springangular.Repository.LivroRepository;
+import com.agsilva.springangular.domain.Categoria;
 import com.agsilva.springangular.domain.Livro;
 import com.agsilva.springangular.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,5 +40,12 @@ public class LivroService {
         newObj.setTitulo(obj.getTitulo());
         newObj.setNome_autor(obj.getNome_autor());
         newObj.setTexto(obj.getTexto());
+    }
+
+    public Livro create(Integer id_cat, Livro obj) {
+        obj.setId(null);
+        Categoria cat= categoriaService.findById(id_cat);
+        obj.setCategoria(cat);
+        return repository.save(obj);
     }
 }
